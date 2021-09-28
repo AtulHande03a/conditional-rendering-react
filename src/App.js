6c232e0023b8bd1319508082ce79e068810f2dc5
Atul Kumar Hande <handeatul03a@gmail.com>
2021-09-28 17:33:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import MultipleReturn from "./MultipleReturn";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <MultipleReturn />
+    </div>
+  );
+}
---
+++ src/MultipleReturn.js
@@ -0,0 +1,9 @@
+const MultipleReturn = () => {
+  return (
+    <>
+      <h3>Multiple Reutrn</h3>
+    </>
+  );
+};
+
+export default MultipleReturn;
